e04c849bad0fa4c87ad8bc1c4ae436080a28b8e7
Nezumi-sama <kryska007@yandex.ru>
2023-02-26 23:35:59 +0300

    Edit build.gradle

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,8 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+//import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 
 class MobileBankApiTestV1 {
     @Test
@@ -17,5 +19,7 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200);
+          // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+//          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
